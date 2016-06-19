1ed0e949286689a7756acaddeb776a46df05a6ab
yellows8 <yellows8@users.noreply.github.com>
2016-06-18 23:13:17 -0400

    Updated comments for varioues fields + ROPBUF, and added a comment about checksum/CRC. Set the counter to 0x1000.

--- stickerhax.s
+++ stickerhax.s
@@ -7,7 +7,9 @@ _start:
 //STACKPIVOT_ADR: "ldm r4, {ip, sp, lr, pc}"
 //ROP_VTABLEFUNCPTR_x10_CALL_R5OBJ: Call vtable funcptr +0x10 from the r5 object.
 
-//ROPBUF: Probably varies per-region. Address of this savefile buffer in the heap.
+//ROPBUF: Address of this savefile buffer in the heap. It's unknown whether this varies per-region, there doesn't seem to be any localization strings in memory prior to this heap addr.
+
+@ The savefile doesn't contain any checksum/CRC.
 
 .word 0x13304f6 @ +0x0 u32: Magicnum 0x13304f6.
 .word 0x714 @ +0x4 u32: Magicnum 0x714.
@@ -15,10 +17,10 @@ _start:
 .string16 "product" @ +0xc 0x10-bytes: UTF-16 string, normally "product".
 .hword 0x0 @ +0x1c u16, Unknown. When updating the savefile this is set to: ((savefile_xef0_u16 * 0x8889) << 4) >> 24.
 .byte 0, 4 @ +0x1e..0x1f: This is the total playtime. byte0 = minutes, byte1 = hours. When updating the savefile the two bytes from savefile+0xef2 are copied to here.
-.word 0 @ +0x20 u32: When updating the savefile the u32 from savefile+0x180 is copied to here. This is a bitmask with 5 bits, presumably for the stickers displayed on the save-select screen.
-.word 0 @ +0x24, unknown.
+.word 0 @ +0x20 u32: When updating the savefile the u32 from savefile+0x180 is copied to here. This is a bitmask with 5 bits, for the stickers displayed on the save-select screen.
+.word 0 @ +0x24, Unknown.
 .word 0xEA8329E0, 0x78 @ +0x28 u64: Timestamp in the usual format(milliseconds since January 1, 2000). This is updated each time the savefile is updated.
-.word 0 @ +0x30 u32: Counter. When updating the savefile, this is set to <highest counter value from all 3 savefiles> + 1.
+.word 0x1000 @ +0x30 u32: Counter. When updating the savefile, this is set to <highest counter value from all 3 savefiles> + 1. The savefile with the highest counter value is the one that is initially 'selected' on the save-select screen.
 .string "mac_1" @ +0x34 char[]: Some 0x10-byte level string. When updating the savefile, the 0x10-bytes here are cleared first, then this is done: strcpy(savefile+0x34, savefile+0x48). This particular strcpy isn't an issue.
 .word 0 @ +0x44 u32: ?
 
